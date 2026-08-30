1d83fad25988f1c7dfca4de32be6cd09c39c3c7d
pschatzmann <phil.schatzmann@gmail.com>
2026-08-30 08:10:36 +0000

    Deploying to doxygen from @ pschatzmann/TinyGPU@3fc1f9b005c14ca5128b24525f468134d21c3234 🚀

--- search/files_5.js
+++ search/files_5.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['font5x7_2eh_0',['Font5x7.h',['../Font5x7_8h.html',1,'']]],
-  ['framebuffer_2eh_1',['FrameBuffer.h',['../FrameBuffer_8h.html',1,'']]],
-  ['framebuffermonochrome_2eh_2',['FrameBufferMonochrome.h',['../FrameBufferMonochrome_8h.html',1,'']]]
+  ['fonts_2eh_1',['Fonts.h',['../Fonts_8h.html',1,'']]],
+  ['framebuffer_2eh_2',['FrameBuffer.h',['../FrameBuffer_8h.html',1,'']]],
+  ['framebuffermonochrome_2eh_3',['FrameBufferMonochrome.h',['../FrameBufferMonochrome_8h.html',1,'']]]
 ];
